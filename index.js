2506cc6134faae7998379289d48e24608f94b626
bigmurry <hhdiao@siom.ac.cn>
2016-11-14 00:12:19 +0800

    add index.js

---
+++ index.js
@@ -0,0 +1,16 @@
+module.exports = class Ng {
+  constructor () {
+  }
+
+  /*
+   * convert data to real react component
+   * based on components provided by themes
+   *
+   * @param {Path} datadir the path to the data file
+   * @param {Path} themedir the path to the theme file
+   * @return {Component} the final component based on data passed in
+   */
+  start (datadir, themedir) {
+    // TODO
+  }
+}
